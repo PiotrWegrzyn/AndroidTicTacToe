be766090c52bbaedb5bedca03b547a0de010f207
piotr-wegrzyn <piotr.wegrzyn@instream.io>
2021-03-31 15:54:04 +0200

    Working minimax algo.

--- src/main/java/com/example/tictactoe/MainActivity.java
+++ src/main/java/com/example/tictactoe/MainActivity.java
@@ -7,14 +7,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import game.Board;
+
 public class MainActivity extends AppCompatActivity {
-
+    Board board = new Board();
     private Boolean bMove = false;
-
+    int[][] button_map = new int[3][3];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button_map[0][0] = R.id.button00;
+        button_map[0][1] = R.id.button01;
+        button_map[0][2] = R.id.button02;
+        button_map[1][0] = R.id.button10;
+        button_map[1][1] = R.id.button11;
+        button_map[1][2] = R.id.button12;
+        button_map[2][0] = R.id.button20;
+        button_map[2][1] = R.id.button21;
+        button_map[2][2] = R.id.button22;
     }
 
     public void clickedSqr( View v)
@@ -22,19 +33,26 @@ public class MainActivity extends AppCompatActivity {
         Button btn = (Button)v;
         if(btn.getText().equals("."))
         {
-            if(bMove)
-            {
-                btn.setText("X");
-                btn.setTextColor(Color.RED);
+            btn.setText("X");
+            btn.setTextColor(Color.RED);
 
+            for(int i=0; i<button_map.length; i++) {
+                for(int j=0; j<button_map[i].length; j++) {
+                    if(button_map[i][j] == btn.getId()){
+                        board.placeMark(i, j);
+                    }
+                }
             }
-            else
-            {
-                btn.setText("O");
-                btn.setTextColor(Color.BLUE);
 
-            }
-            bMove = !bMove;
+            int[] move = MiniMaxCombined.getBestMove(board);
+            int row = move[0];
+            int col = move[1];
+            board.placeMark(row, col);
+
+            Button btn2 = (Button)findViewById(button_map[row][col]);
+
+            btn2.setText("O");
+            btn2.setTextColor(Color.BLUE);
         }
 
     }
@@ -54,18 +72,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void onReset(View v)
     {
-        setBtn(R.id.button3);
-        setBtn(R.id.button4);
-        setBtn(R.id.button6);
-        setBtn(R.id.button7);
-        setBtn(R.id.button8);
-        setBtn(R.id.button9);
-        setBtn(R.id.button10);
-        setBtn(R.id.button11);
-        setBtn(R.id.button12);
+        board = new Board();
+        for(int i=0; i<button_map.length; i++) {
+            for(int j=0; j<button_map[i].length; j++) {
+                setBtn(button_map[i][j]);
+            }
+        }
+
 
     }
-
-
 
 }
